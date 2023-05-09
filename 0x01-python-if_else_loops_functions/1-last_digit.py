003688f6f998be8c56176c698d8ded2bea646d4f
Waled Wael <waledwael0@gmail.com>
2023-05-09 13:02:58 -0700

    Done

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,15 +1,13 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
+digit = abs(number) % 10
 if number < 0:
-    last_digit = -number % 10
-    last_digit = -last_digit
+    digit = -digit
+print("Last digit of {} is {} and is ".format(number, digit), end="")
+if digit > 5:
+    print("greater than 5")
+elif digit == 0:
+    print("0")
 else:
-    last_digit = number % 10
-if last_digit == 0:
-    print(f"Last digit of {number} is {last_digit} and is 0")
-elif last_digit < 6:
-    print(f"Last digit of {number}\
-    is {last_digit} and is less than 6 and not 0")
-else:
-    print(f"Last digit of {number} is {last_digit} and is greater than 5")
+    print("less than 6 and not 0")
--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,5 @@
-#!/bin/usr/python3
+#!/usr/bin/python3
 for i in range(97, 123):
     if i == 101 or i == 113:
         i = i + 1
-    print(chr(i), end='')
+     print("{}".format(chr(letter)), end="")
